d7f4013e60cb2e657977e7e5d79e0b5acb58007b
Ilya Markov <imarkovkin@gmail.com>
2022-11-26 21:07:56 +0300

    Добавьте файлы проекта.

---
+++ Lab 17/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_17
+{
+    class BankAccount<T>
+    {
+        public T Number {get;set;}
+        public double Balance { get; set; }
+        public string Owner { get; set; }       
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            Number = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+
+            Console.WriteLine("Введите баланс счета");
+            Balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО владельца счета");
+            Owner = Console.ReadLine();
+
+        }
+        public string Getinfo()
+        {
+            return $"Номер счета: {Number}. Баланс: {Balance}. Владелец: {Owner}";
+        }
+
+
+    }
+}
---
+++ Lab 17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> acc1 = new BankAccount<int>();
+            BankAccount<string> acc2 = new BankAccount<string>();
+            acc1.Input();
+            Console.WriteLine(acc1.Getinfo());
+            acc2.Input();
+            Console.WriteLine(acc2.Getinfo());
+            Console.ReadKey();
+
+        }
+    }
+}
